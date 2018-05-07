c760d123b9bd3905f515ed3cbc1fe82b21dd0e3a
doucai <hyu1993@sina.com>
2018-05-07 11:28:06 +0800

    社区，上栏部分

--- pages/community/community.js
+++ pages/community/community.js
@@ -5,14 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    userName:'',
+    catName:'',
+    catType:'',
+    sex:'',
+    catAttr:'',
+    ownImg:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.request({
+      url: "https://www.easy-mock.com/mock/5ae2b3c4ab5bad29ce810a51/example/xiaocai",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
   },
 
   /**
