736d96a879c85460a886b5e18b23065f62f94775
tianda <hyu1993@sina.com>
2018-05-08 16:26:23 +0800

    topbar组件

--- components/timeline/topbar/topbar.js
+++ components/timeline/topbar/topbar.js
@@ -1,23 +1,61 @@
-// components/topbar/topbar.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+// pages/community/community.js
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-
+    userName:'',
+    catName:'',
+    catType:'',
+    sex:'',
+    catAttr:'',
+    ownImg:'',
+    isShow:true,
+    showView:false
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
+  onLoad: function (options) {
+    wx.request({
+      url: "https://www.easy-mock.com/mock/5ae2b3c4ab5bad29ce810a51/example/xiaocai",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+  },
 
-  }
+  // 点击跳转到。。。
+  routerTo:()=>{
+    wx.redirectTo({
+      url: '/pages/home/home'
+    })
+  },
+  // 发帖
+  sendTo:()=>{
+    console.log("发帖")
+  },
+  
+  // 滚动时触发
+  scroll: function(){
+    var that = this;
+    that.setData({
+      isShow: false,
+      showView:true
+    })
+  },
+  // 到顶部后触发
+  upper: function () {
+    console.log("123")
+    var that = this;
+    that.setData({
+      isShow: true,
+      showView: false
+    })
+  },
 })
