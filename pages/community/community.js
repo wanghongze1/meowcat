8e2115b979536775d33b5b8a12778362e036fea1
FLQ77 <1249003920@qq.com>
2018-05-08 09:55:01 +0800

    热门时间轴页面

--- pages/community/community.js
+++ pages/community/community.js
@@ -5,21 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    catArray: [{ id: 1, img:'../../images/cat_1.jpg',name:'咖啡猫儿',time:'2018.5.01',owner:'阳叔'},
+      { id: 2, img: '../../images/cat_2.jpg', name: '虎汴', time: '2018.4.28', owner: '错过' },
+      { id: 3, img: '../../images/cat_3.jpg',name: '噗噗', time: '2018.4.27', owner: '产品萌新-新月' }
+      ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.request({
+      url: 'test.php',
+      data:{},
+      header:{
+        'content-type':'application/json'
+      },
+      success: function(res) {
+        console.log(res);
+      },
+      fail:function (err) {
+        console.log(err)
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    wx.setNavigationBarTitle({
+      title: '喵与猫寻',
+    }),
+    wx.setNavigationBarColor({
+        frontColor: '#ffffff',
+        backgroundColor: '#cd9054',
+    })
   },
 
   /**
@@ -62,5 +83,17 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  //发帖
+  post: function () {
+    wx.navigateTo({
+      url: '',
+    })
+  },
+  //卡片
+  card: function () {
+    wx.navigateTo({
+      url: '',
+    })
   }
 })
